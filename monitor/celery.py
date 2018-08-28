72182905f731ce3d8c11e26c113c2776ab6d2f0c
alexpl52 <apolishchuk52@gmail.com>
2018-08-29 03:57:15 +0700

    Add Celery

--- monitor/celery.py
+++ monitor/celery.py
@@ -5,7 +5,7 @@ from celery import Celery
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'picker.settings')
 
-app = Celery('proj')
+app = Celery('picker')
 
 # Using a string here means the worker doesn't have to serialize
 # the configuration object to child processes.
