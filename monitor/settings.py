ad24f5dd237eb77ac327ee9d8cfd655b73c9f40c
alexpl52 <apolishchuk52@gmail.com>
2018-08-29 03:44:53 +0700

    Add Celery and  django-celery-results

--- monitor/settings.py
+++ monitor/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_celery_results',
+
+    'picker',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Celery
+CELERY_RESULT_BACKEND = 'django-db'
