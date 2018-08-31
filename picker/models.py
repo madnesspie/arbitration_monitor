7a5b92a67fbe6c102d4d00a4b144362bfc67be2d
alexpl52 <apolishchuk52@gmail.com>
2018-09-01 04:28:12 +0700

    Add Binance ws-ticker and add models

--- picker/models.py
+++ picker/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Exchange(models.Model):
+    name = models.CharField(max_length=100)
+    separator = models.CharField(max_length=1)
+
+
+class CurrencyPair(models.Model):
+    exchange = models.ForeignKey(Exchange, on_delete=models.CASCADE)
+    base = models.CharField(max_length=10)
+    quoted = models.CharField(max_length=10)
+
+
+class FinancialQuote(models.Model):
+    pair = models.ForeignKey(CurrencyPair)
---
+++ picker/wrappers/bittrex.py
@@ -0,0 +1,13 @@
+from bittrex.bittrex import Bittrex
+
+"""
+Key
+99a2a342bf41404995bc9ae2eef449d3 
+Secret
+239d000ff4424a508825c0edb4eef0f1
+"""
+
+client = Bittrex()
+
+
+
